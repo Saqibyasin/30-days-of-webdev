1c5a0ecb42cbe53ee69505bba1659ab98cd15ade
Kifayat Yaseen Bhat <kifayatyaseenbhat@gmail.com>
2026-04-26 17:59:22 +0530

    added some js functionality

--- Day25-Final-Portfolio/script.js
+++ Day25-Final-Portfolio/script.js
@@ -1,20 +1,45 @@
-// // LOADER
-// window.addEventListener("load", () => {
-//   const loader = document.getElementById("loader");
-//   loader.style.opacity = "0";
-//   setTimeout(() => loader.style.display = "none", 500);
-// });
+window.addEventListener('load', () => {
+    const loader = document.querySelector('#loader');
+    setTimeout(() => {
+        loader.style.opacity = '0';
+        setTimeout(() => {
+            loader.style.display = 'none';
+        }, 500);
+    }, 1000);
 
-// // NAVBAR SCROLL
-// window.addEventListener("scroll", () => {
-//   const navbar = document.getElementById("navbar");
-//   navbar.classList.toggle("scrolled", window.scrollY > 50);
-// });
+})
 
-// // HAMBURGER
-// const hamburger = document.querySelector(".hamburger");
-// const navLinks = document.querySelector(".nav-links");
+const navbar = document.querySelector('#navbar');
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
 
-// hamburger.addEventListener("click", () => {
-//   navLinks.classList.toggle("active");
-// });
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 50) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
+
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    hamburger.classList.toggle('active');
+});
+
+document.querySelectorAll('a').forEach(link => {
+    link.addEventListener('click', () => {
+        navLinks.classList.remove('active');
+        hamburger.classList.remove('active');
+    })
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      target.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
